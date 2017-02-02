5d706d6b8e2f51b3ed580ab97f1e7325eb366179
Ryan Gabriel Bunquin <bunquinryan@gmail.com>
2017-02-03 01:37:46 +0800

    cleaning up

--- application/views/view_registration.php
+++ application/views/view_registration.php
@@ -113,15 +113,9 @@
                             <h2 class="fs-title">Body Details</h2>
 
                             <h6 class="error" id="errorHeight"></h6>
-                            <input type="text" name="height" placeholder="Height in Feet" />
-                            <h6 class="error" id="errorWeight"></h6>
-                            <input type="text" name="weight" placeholder="Weight in Lbs" />
-
-                            <h6 class="error">Input your height!</h6>
                             <input type="text" name="height" placeholder="Height in cm" />
-                            <h6 class="error">Input your weight!</h6>
+                            <h6 class="error" id="errorWeight"></h6>
                             <input type="text" name="weight" placeholder="Weight in Kgs" />
-
                             <input type="button" name="previous" class="previous action-button" value="Previous" />
                             <input type="submit" name="submit" class="submit action-button" value="Submit" />
                         </fieldset>
